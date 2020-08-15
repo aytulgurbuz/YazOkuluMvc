2d889f055665d4c9f127eb1cd11dfa33180feb37
aytulgurbuz <65277922+aytulgurbuz@users.noreply.github.com>
2020-08-15 16:35:55 +0300

    2.Commit
    Veri tabanı bağlantısı yaptım.Kategorileri anasayfada listeledim.
    Admin panelinde makale listeleme,ekleme yaptım.

---
+++ Controllers/AdminMakaleController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcBlog.Models;
+using System.IO;
+using System.Web.Helpers;
+
+namespace MvcBlog.Controllers
+{
+    public class AdminMakaleController : Controller
+    {
+        mvcblogDB db = new mvcblogDB();
+        // GET: AdminMakale
+        public ActionResult Index()
+        {
+            var makales = db.Makales.ToList();
+            return View(makales);
+        }
+
+        // GET: AdminMakale/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: AdminMakale/Create
+        public ActionResult Create()
+        {
+            ViewBag.Kategoriid = new SelectList(db.Kategoris, "Kategoriid", "KategoriAd");
+            return View();
+        }
+
+        // POST: AdminMakale/Create
+        [HttpPost]
+        public ActionResult Create(Makale makale, string etiketler, HttpPostedFileBase Foto)
+        {
+            if (ModelState.IsValid)
+            {
+                if (Foto != null)
+                {
+                    WebImage img = new System.Web.Helpers.WebImage(Foto.InputStream);
+                    FileInfo fotoinfo = new FileInfo(Foto.FileName);
+
+                    string newfoto = Guid.NewGuid().ToString() + fotoinfo.Extension;
+                    img.Resize(800, 350);
+                    img.Save("~/Uploads/MakaleFoto/" + newfoto);
+                    makale.Foto = "/Uploads/MakaleFoto/" + newfoto;
+
+
+                }
+                if (etiketler != null)
+                {
+                    string[] etiketdizi = etiketler.Split(',');
+                    foreach (var i in etiketdizi)
+                    {
+                        var yenietiket = new Etiket { EtiketAdi = i };
+                        db.Etikets.Add(yenietiket);
+                        makale.Etikets.Add(yenietiket);
+                    }
+                }
+                makale.UyeId = Convert.ToInt32(Session["uyeid"]);
+                makale.Okunma = 1;
+                db.Makales.Add(makale);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(makale);
+
+        }
+
+        // GET: AdminMakale/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: AdminMakale/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AdminMakale/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: AdminMakale/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcBlog.Models;
 
 namespace MvcBlog.Controllers
 {
     public class HomeController : Controller
     {
+        mvcblogDB db = new mvcblogDB();
         // GET: Home
         public ActionResult Index()
         {
@@ -20,6 +22,10 @@ namespace MvcBlog.Controllers
         public ActionResult iletisim()
         {
             return View();
+        }
+        public ActionResult KategoriPartial()
+        {
+            return View(db.Kategoris.ToList());
         }
     }
 }
